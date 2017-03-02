0e37b96fc08f6da8be82ec308f46123f91b1adc7
moniaaaaa10 <moniaaaaa10@wp.pl>
2017-03-02 19:04:49 +0100

    kalkulator

---
+++ test/aa/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float  e;
+    cin >> e ;
+
+
+
+    float  f;
+    cin >> f ;
+    cout << f + e << endl;
+
+    return 0;
+}
---
+++ test/test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int x = 10;
+    cout << x + 15;
+    return 0;
+}
